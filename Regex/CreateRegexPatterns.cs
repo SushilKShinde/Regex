659166a5134658497c4baef3fe92b43084dc35d6
Sushil <skshinde2028@gmail.com>
2023-05-11 15:50:47 +0530

    [Sushil] validating Phone number with regex

---
+++ Regex/CreateRegexPatterns.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+namespace RegexPatterns
+{
+    internal class CreateRegexPatterns
+    {
+        public static void PhoneNumberPattern()
+        {
+            string[] input = { "8407958538", "9561665172", "7541457562" };
+            string pattern = "[8 9][0-9]{9}";  // starts with 8 or 9 , contains 9 numbers betn 0 to 9 
+            Console.Write("\ninput phone numbers are: ");
+            foreach( string s in input)
+            {
+                Console.Write($"{s} ");
+            }
+            Console.WriteLine();
+            foreach(string s in input)
+            {
+                if(Regex.IsMatch(s,pattern)) // return true or false 
+                {
+                    Console.WriteLine($"Valid Phone Number: {s}");
+                }
+            }   
+        }
+     
+    }
+}
--- Regex/Program.cs
+++ Regex/Program.cs
@@ -4,13 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Regex
+namespace RegexPatterns
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the regex (regular expressio) implementation ");
+            Console.WriteLine("Choose a Option from below");
+            Console.WriteLine("1.Validate Phone Number\n2.Validate First name ");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    CreateRegexPatterns.PhoneNumberPattern();
+                    break;
+                case 2:
+
+                    break;
+                default:
+                    Console.WriteLine("Choose valid option");
+                    break;
+            }
+
         
         }
     }
