46641c721007ac9e9ba4ea8b8e84bd76799047d1
Sushil <skshinde2028@gmail.com>
2023-05-11 16:57:07 +0530

    [Sushil] validating email id with regex

--- Regex/CreateRegexPatterns.cs
+++ Regex/CreateRegexPatterns.cs
@@ -10,21 +10,12 @@ namespace RegexPatterns
     {
         public static void PhoneNumberPattern()
         {
-            string[] input = { "8407958538", "9561665172", "7541457562" };
+            string[] inputs = { "8407958538", "9561665172", "7541457562" };
             string pattern = "[8 9][0-9]{9}";  // starts with 8 or 9 , contains 9 numbers betn 0 to 9 
             Console.Write("\ninput phone numbers are: ");
-            foreach( string s in input)
-            {
-                Console.Write($"{s} ");
-            }
+            PrintInputs(inputs);
             Console.WriteLine();
-            foreach(string s in input)
-            {
-                if(Regex.IsMatch(s,pattern)) // return true or false 
-                {
-                    Console.WriteLine($"Valid Phone Number: {s}");
-                }
-            }   
+            IterateOverLoop(inputs, pattern);
         }
 
         public static void ValidateFirstName()
@@ -33,11 +24,26 @@ namespace RegexPatterns
             string[] inputs = { "Sushil", "Sachin", "Mahesh", "amit" };
             Console.Write("Input names are: ");
             PrintInputs(inputs);
+            Console.WriteLine(); 
             Console.WriteLine("Valid names are: ");
             IterateOverLoop(inputs, pattern);
 
         }
 
+        public static void ValidateEmailId()
+        {
+            // allows alphanumeric characters, dots, hyphens
+            //^ represents start of string and $ end of string
+            //+ at least one occurence should be there
+            string pattern = "^[a-z0-9_+.]+@[a-zA-Z0-9_]+[.][a-zA-Z]{2,3}$";     //sks.123@gmail.com
+            string[] inputs = { "sks.123@gmail.com", "123@hotmail.in","Sushil@gmail.com", "sks@gmailcom" };
+            Console.Write("Input email ids are : ");
+            PrintInputs(inputs);
+            Console.WriteLine();
+            Console.WriteLine("Valid email ids are: ");
+            IterateOverLoop(inputs, pattern);
+
+        } 
         public static void PrintInputs(string[] inputs)
         {
             foreach(string s in inputs)
@@ -58,10 +64,7 @@ namespace RegexPatterns
                 {
                     Console.WriteLine($"Invalid input: {s}");
                 }
-                
-
             }
         }
-     
     }
 }
--- Regex/Program.cs
+++ Regex/Program.cs
@@ -12,7 +12,7 @@ namespace RegexPatterns
         {
             Console.WriteLine("Welcome to the regex (regular expressio) implementation ");
             Console.WriteLine("Choose a Option from below");
-            Console.WriteLine("1.Validate Phone Number\n2.Validate First name ");
+            Console.WriteLine("1.Validate Phone Number\n2.Validate First name\n3.Validate Email Id ");
             int option = Convert.ToInt32(Console.ReadLine());
 
             switch (option)
@@ -22,6 +22,9 @@ namespace RegexPatterns
                     break;
                 case 2:
                     CreateRegexPatterns.ValidateFirstName();
+                    break;
+                case 3:
+                    CreateRegexPatterns.ValidateEmailId();
                     break;
                 default:
                     Console.WriteLine("Choose valid option");
